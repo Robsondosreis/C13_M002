2297745b86574bd293f3566c3e8b2054dd94068c
Robsondosreis <rob_reis@outlook.com>
2022-04-27 20:32:58 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,111 @@
+import dotenv from "dotenv";dotenv.config();
+import express from "express"; // importando express
+import path from "path"; // serve para definir caminhos padrões
+
+const __dirname = path.resolve(path.dirname("")); //__dirname serve para informar qual é o caminho padrão da minha pasta
+
+const app = express(); //intanciando o express dentro da const
+
+app.use(express.urlencoded({ extended: true })); // o corpo (body) da requisição
+app.use(express.json()); // converter as info para json
+
+app.set("view engine", "ejs"); // faz com que o express reconheça o ejs como motor de visualização
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3005;
+app.listen(port, () => {
+  //listen é uma função do express para criar servidor
+  console.log(`Rodando na porta ${port} `);
+});
+
+let pokemon = [
+  {
+    id: 1,
+    nome: "Bulbasaur",
+    tipo1: "Planta",
+    altura: "0,7",
+    peso: "6,9",
+    categoria: "Semente",
+    habilidades: "Superar",
+    genero: "Macho, Femêa",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+  },
+  {
+    id: 2,
+    nome: "Charmander",
+    tipo1: "Fogo",
+    altura: "0,6",
+    peso: "8,5",
+    categoria: "Lagarto",
+    habilidades: "Chama",
+    genero: "Macho, Femêa",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  },
+  {
+    id: 3,
+    nome: "Squirtle",
+    tipo1: "Agua",
+    altura: "0,5",
+    peso: "9,0",
+    categoria: "Tartaruga",
+    habilidades: "Torrente",
+    genero: "Macho, Femêa",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  },
+  {
+    id: 4,
+    nome: "Cyndaquil ",
+    tipo1: "Fogo",
+    altura: "0,5",
+    peso: "7,9",
+    categoria: "Rato de fogo",
+    habilidades: "Chama",
+    genero: "Macho, Femêa",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png",
+  },
+];
+
+app.get("/", (req, res) => {
+  //get é um metodo HTTP/HTTPS que serve para trazer uma pagina
+  res.render("index.ejs", {
+    pokemon,
+  });
+});
+
+app.get("/detalhes/:id", (req, res) => {
+  req.params.id;
+  let poke = [];
+  pokemon.filter((element) => {
+    if (element.id == req.params.id) {
+      poke = element;
+    }
+  });
+
+  res.render("detalhes.ejs", {
+    poke,
+  });
+});
+
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro.ejs");
+});
+
+app.post("/cadastro", (req, res) => {
+  let i = pokemon[pokemon.length - 1].id + 1;
+
+  const { nome, tipo1, altura, peso, categoria, habilidades, genero, img } =
+    req.body;
+  pokemon.push({
+    id: i,
+    nome,
+    tipo1,
+    altura,
+    peso,
+    categoria,
+    habilidades,
+    genero,
+    img,
+  });
+  console.log(pokemon);
+  res.redirect("/");
+});
---
+++ readme.md
@@ -0,0 +1,23 @@
+# pokedex
+
+## descrição
+
+este projeto foi ralizado com fins de aprendizado no curso de Fuulstack da BlueEdtech no ano de 2022 - modulo 2
+
+## Stacks
+
+As ferramentas usadas para este projeto são:
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+
+
+## contato
+
+<a href="https://www.linkedin.com/in/robson-dos-reis-319327234" target="blank" 
+rel="noopener">![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.
+svg?style=for-the-badge&logo=linkedin&logoColor=white)<a>
+
